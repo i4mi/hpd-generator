a9cda2e57140b58c82d4e5ffa53756991c4eaed0
heg2 <heg2@bots.github.com>
2024-08-29 12:24:22 +0000

    updated GitHub Pages

--- server.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-const port = process.env.PORT || 3000;
-const app = express();
-
-app.use(express.static('dist'));
-
-app.listen(port, '0.0.0.0',() => {
-    console.log(`HPD Generator listening at http://0.0.0.0:${port}`);
-});
--- src/types.js
+++
@@ -1,4 +0,0 @@
-export const TYPES = {
-    group: ['Ärzte', 'Pflege', 'Therapeuten', 'Assistenzärzte', 'Operationsassistenzen'],
-    department: ['Medizin', 'Chirurgie','Kardiologie', 'Neurochirurgie', 'Traumatologie', 'Endokrinologie']
-}
